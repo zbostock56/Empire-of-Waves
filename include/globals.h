04d317f7d6406071a07729847f374c2c1fd3eb20
jbs <saysanabjack@gmail.com>
2023-09-25 11:03:57 -0400

    Added global game state

--- include/globals.h
+++ include/globals.h
@@ -1,15 +1,23 @@
+// GLOBAL GAME STATE
+typedef enum game_mode {
+  EXPLORATION,
+  COMBAT
+} GAME_MODE;
+extern GAME_MODE mode;
+extern float delta_time;
+
 // EXPLORATION MODE STATE
 extern E_PLAYER e_player;
-CHUNK player_chunks[9];
-TRADE_SHIP *trade_ships;
+extern CHUNK player_chunks[9];
+extern TRADE_SHIP *trade_ships;
 // Each trade ship contains a single loaded chunk
 // Each chunk corresponds to the trade ship of the same
 // index in trade_ships
-CHUNK *trade_ship_chunks;
-unsigned int num_trade_ships;
-float next_event;
+extern CHUNK *trade_ship_chunks;
+extern unsigned int num_trade_ships;
+extern float next_event;
 
 // COMBAT MODE STATE
-C_PLAYER c_player;
-C_UNIT *npc_units;
-unsigned int num_npc_units;
+extern C_PLAYER c_player;
+extern C_UNIT *npc_units;
+extern unsigned int num_npc_units;
